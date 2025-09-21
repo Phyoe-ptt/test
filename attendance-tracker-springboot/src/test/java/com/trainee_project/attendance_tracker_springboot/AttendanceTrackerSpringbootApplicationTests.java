4aec58491c957f03cc8e79b79611c54279221640
Phyoe, Thuta <phyoethuta98@gmail.com>
2025-09-21 09:10:20 +0700

    test23

--- attendance-tracker-springboot/src/test/java/com/trainee_project/attendance_tracker_springboot/AttendanceTrackerSpringbootApplicationTests.java
+++ attendance-tracker-springboot/src/test/java/com/trainee_project/attendance_tracker_springboot/AttendanceTrackerSpringbootApplicationTests.java
@@ -2,12 +2,15 @@ package com.trainee_project.attendance_tracker_springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("ci")   // use H2-based CI profile
 @SpringBootTest
 class AttendanceTrackerSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // passes if the Spring context starts with the 'ci' profile
+    }
 }
+
